288886ec23b9a251263844b058b1949934447efd
mogamoga <mogamoga.g1thu6@gmail.com>
2024-02-01 19:26:18 +0900

    初めてのVueとビルド ドキドキ！！

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development', // production
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: 'true',
+      __VUE_PROD_DEVTOOLS__: 'false',
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+    })
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm-bundler.js'
+    }
+  },
+};
