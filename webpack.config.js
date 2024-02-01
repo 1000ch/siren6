bae2e524b3a3e4eeb26b6931d3fd27441b7ef468
mogamoga <mogamoga.g1thu6@gmail.com>
2024-02-01 22:33:06 +0900

    ホットリロードができる 神！！！

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development', // production
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
     filename: 'main.js'
   },
   devServer: {
