56878f37c1014b3ad953c5bbdc40cce9066c3be6
mogamoga <mogamoga.g1thu6@gmail.com>
2024-02-01 21:57:10 +0900

    ok

--- src/main.js
+++ src/main.js
@@ -3,7 +3,24 @@ import { createApp } from 'vue'
 const vm = {
     data() {
         return {
-            message: 'Hello Vue!'
+            itemGroupKey: ['kusa', 'tue', 'makimono', 'tubo', 'okou', 'udewa', 'buki', 'tate'],
+            itemGroupName: {
+                kusa: '草', tue: '杖',
+                makimono: '巻物', tubo: '壺',
+                okou: 'お香', udewa: '腕輪',
+                buki: '武器', tate: '盾',
+            }
+        }
+    },
+    created() {
+        // 仮
+    },
+    methods: {
+        onClickItemGroup(key) {
+            console.log(key);
+        },
+        onClickNedanType() {
+
         }
     }
 };
