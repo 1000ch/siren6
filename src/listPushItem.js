25a0e5b66b7a128e022f1a19767c6d96f816d412
mogamoga <mogamoga.g1thu6@gmail.com>
2024-02-02 09:48:41 +0900

    ぶぶんてきよおおおおおおおおおお！！！！！！

--- src/listPushItem.js
+++ src/listPushItem.js
@@ -1,21 +1,25 @@
 
-export function listPushItem(list, name, kaine, urine) {
-    list.push({
-        status: 'normal',
-        name: name,
-        kaine: kaine,
-        urine: urine
-    });
-    list.push({
-        status: 'syukufuku',
-        name: name + '🔔',
-        kaine: kaine * 2,
-        urine: urine * 2
-    });
-    list.push({
-        status: 'noroi',
-        name: name + '💀',
-        kaine: Math.floor(kaine * 0.87),
-        urine: Math.floor(urine * 0.87)
-    });
+export function createFuncListPushItem(list, canSyukufuku) {
+    return function(name, kaine, urine) {
+        list.push({
+            status: 'normal',
+            name: name,
+            kaine: kaine,
+            urine: urine
+        });
+        if (canSyukufuku) {
+            list.push({
+                status: 'syukufuku',
+                name: name + '🔔',
+                kaine: kaine * 2,
+                urine: urine * 2
+            });
+        }
+        list.push({
+            status: 'noroi',
+            name: name + '💀',
+            kaine: Math.floor(kaine * 0.87),
+            urine: Math.floor(urine * 0.87)
+        });
+    }
 }
---
+++ src/tue.js
@@ -0,0 +1,5 @@
+import { listPushItem } from './listPushItem'
+
+export const tueList = [];
+
+
