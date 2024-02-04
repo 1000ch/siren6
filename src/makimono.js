374e0354ec11018f7f521767e794f894fba38f1b
mogamoga <mogamoga.g1thu6@gmail.com>
2024-02-04 12:41:53 +0900

    選択式の巻物 分かりやすく

--- src/makimono.js
+++ src/makimono.js
@@ -1,25 +1,28 @@
-import { createFuncItemListPush } from './item'
 
 export const makimonoList = [];
-const itemListPush = createFuncItemListPush(makimonoList, true);
+
+function itemListPush(name, kaine, urine, needSelect = false) {
+    makimonoList.push({name, kaine, urine, unused: true, needSelect});
+}
+
 itemListPush('混乱の巻物', 300, 120);
 itemListPush('バクスイの巻物', 300, 120);
 itemListPush('ゾワゾワの巻物', 300, 120);
 itemListPush('真空斬りの巻物', 300, 120);
 itemListPush('全滅の巻物', 3000, 1200);
-itemListPush('おはらいの巻物', 600, 240);
-itemListPush('識別の巻物', 300, 120);
-itemListPush('天の恵みの巻物', 400, 160);
-itemListPush('地の恵みの巻物', 400, 160);
-itemListPush('メッキの巻物', 400, 160);
-itemListPush('印増大の巻物', 400, 160);
-itemListPush('印消しの巻物', 400, 160);
-itemListPush('銀封印の巻物', 300, 120);
-itemListPush('銀はがしの巻物', 1000, 400);
-itemListPush('壺増大の巻物', 1000, 400);
-itemListPush('吸い出しの巻物', 1000, 400);
-itemListPush('おにぎりの巻物', 400, 160);
-itemListPush('呪いの巻物', 1000, 400);
+itemListPush('おはらいの巻物', 600, 240, true);
+itemListPush('識別の巻物', 300, 120, true);
+itemListPush('天の恵みの巻物', 400, 160, true);
+itemListPush('地の恵みの巻物', 400, 160, true);
+itemListPush('メッキの巻物', 400, 160, true);
+itemListPush('印増大の巻物', 400, 160, true);
+itemListPush('印消しの巻物', 400, 160, true);
+itemListPush('銀封印の巻物', 300, 120, true);
+itemListPush('銀はがしの巻物', 1000, 400, true);
+itemListPush('壺増大の巻物', 1000, 400, true);
+itemListPush('吸い出しの巻物', 1000, 400, true);
+itemListPush('おにぎりの巻物', 400, 160, true);
+itemListPush('呪いの巻物', 1000, 400, true);
 itemListPush('あかりの巻物', 600, 240);
 itemListPush('迷子の巻物', 600, 240);
 itemListPush('罠消しの巻物', 600, 240);
