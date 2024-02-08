b6becc77785b0138a9df04842ed84ba7a0557f9b
mogamoga <mogamoga.g1thu6@gmail.com>
2024-02-08 21:40:42 +0900

    はえはえ

---
+++ test/test.mjs
@@ -0,0 +1,9 @@
+import assert from 'assert';
+
+describe('草', function () {
+    describe('hogehoge', function () {
+        it('hogehoge', function () {
+            assert.equal(1, 2);
+        });
+    });
+});
