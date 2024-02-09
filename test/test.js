deb6bd81a172c3d0a063be901ad972477920e4df
mogamoga <mogamoga.g1thu6@gmail.com>
2024-02-09 09:24:11 +0900

    テストケースを書いた

--- test/test.js
+++ test/test.js
@@ -1,9 +1,106 @@
 import { assert } from 'chai';
 import { findAllItemList, findItemList } from '../src/item/item.js';
 
+// assert.strictEqual(1, 1);
+
 describe('草', function () {
     it('検索 通常', function () {
-        // assert.strictEqual(1, 1);
+        assert.fail();
+    });
+    it('検索 呪い', function () {
+        assert.fail();
+    });
+    it('検索 祝福', function () {
+        assert.fail();
+    });
+    it('検索 該当なし', function () {
+        assert.fail();
+    });
+    it('検索 一覧', function () {
+        assert.fail();
+    });
+});
+
+describe('杖', function () {
+    it('検索 通常', function () {
+        assert.fail();
+    });
+    it('検索 呪い', function () {
+        assert.fail();
+    });
+    it('検索 祝福は存在しない', function () {
+        assert.fail();
+    });
+    it('検索 該当なし', function () {
+        assert.fail();
+    });
+    it('検索 一覧', function () {
+        assert.fail();
+    });
+});
+
+describe('巻物', function () {
+    it('検索 通常', function () {
+        assert.fail();
+    });
+    it('検索 呪い', function () {
+        assert.fail();
+    });
+    it('検索 祝福', function () {
+        assert.fail();
+    });
+    it('検索 該当なし', function () {
+        assert.fail();
+    });
+    it('検索 一覧', function () {
+        assert.fail();
+    });
+});
+
+describe('壺', function () {
+    it('検索 通常', function () {
+        assert.fail();
+    });
+    it('検索 呪い', function () {
+        assert.fail();
+    });
+    it('検索 祝福は存在しない', function () {
+        assert.fail();
+    });
+    it('検索 該当なし', function () {
+        assert.fail();
+    });
+    it('検索 一覧', function () {
+        assert.fail();
+    });
+});
+
+describe('腕輪', function () {
+    it('検索 通常', function () {
+        assert.fail();
+    });
+    it('検索 呪い', function () {
+        assert.fail();
+    });
+    it('検索 祝福は存在しない', function () {
+        assert.fail();
+    });
+    it('検索 該当なし', function () {
+        assert.fail();
+    });
+    it('検索 一覧', function () {
+        assert.fail();
+    });
+});
+
+describe('杖+', function () {
+    it('検索 通常', function () {
+        assert.fail();
+    });
+    it('検索 呪い', function () {
+        assert.fail();
+    });
+    it('検索 該当なし', function () {
         assert.fail();
     });
 });
