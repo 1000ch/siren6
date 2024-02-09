2034a35933c245159bdb84abfe38e3f59e728060
mogamoga <mogamoga.g1thu6@gmail.com>
2024-02-09 16:17:19 +0900

    不安も吹き飛ばしていこう

--- test/kusa.js
+++ test/kusa.js
@@ -5,11 +5,13 @@ import { kusaList } from '../src/item/kusa.js';
 describe('草', function () {
     it('検索 通常 買値', function () {
         const resultList = findItemList(kusaList, 50, 'kaine');
+        assert.strictEqual(resultList.length, 2);
         assert.strictEqual(resultList[0].name, '毒草');
         assert.strictEqual(resultList[1].name, '暴走の種');
     });
     it('検索 通常 売値', function () {
         const resultList = findItemList(kusaList, 25, 'urine');
+        assert.strictEqual(resultList.length, 6);
         assert.strictEqual(resultList[0].name, '混乱草');
         assert.strictEqual(resultList[1].name, '睡眠草');
         assert.strictEqual(resultList[2].name, '目つぶし草');
@@ -18,10 +20,15 @@ describe('草', function () {
         assert.strictEqual(resultList[5].name, 'パワーアップ草');
     });
     it('検索 呪い 買値', function () {
-        assert.fail();
+        const resultList = findItemList(kusaList, 87, 'kaine');
+        assert.strictEqual(resultList.length, 2);
+        assert.strictEqual(resultList[0].name, '高飛び草💀');
+        assert.strictEqual(resultList[1].name, 'くねくね草💀');
     });
     it('検索 呪い 売値', function () {
-        assert.fail();
+        const resultList = findItemList(kusaList, 87, 'urine');
+        assert.strictEqual(resultList.length, 1);
+        assert.strictEqual(resultList[0].name, 'ドラゴン草💀');
     });
     it('検索 祝福 買値', function () {
         assert.fail();
