135e3be03b5948f9e4a9dd6a03a92abe9eb0816e
mogamoga <mogamoga.g1thu6@gmail.com>
2024-02-25 17:30:36 +0900

    意外なものも 変なものも あの日の涙も 全部があなたを作ったんでしょ 辞めるなんてもったいない

--- src/main.js
+++ src/main.js
@@ -164,11 +164,17 @@ const vm = {
             }
             else {
                 this.resultItemList = repo.findItemList(this.searchNedan, this.searchNedanType);
-                if (this.resultItemList.length === 0) {
-                    this.resultItemList = repo.findItemList(this.searchNedan, this.searchNedanType === 'kaine' ? 'urine' : 'kaine');
-                    if (this.resultItemList.length > 0) {
-                        this.shouldDisplaySearchHint = true;
+
+                if (this.shouldKubetuKaineAndUrine) {
+                    if (this.resultItemList.length === 0) {
+                        this.resultItemList = repo.findItemList(this.searchNedan, this.searchNedanType === 'kaine' ? 'urine' : 'kaine');
+                        if (this.resultItemList.length > 0) {
+                            this.shouldDisplaySearchHint = true;
+                        }
                     }
+                }
+                else {
+                    this.resultItemList = this.resultItemList.concat(repo.findItemList(this.searchNedan, this.searchNedanType === 'kaine' ? 'urine' : 'kaine'));
                 }
             }
         },
