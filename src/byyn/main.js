d7a0b7b07d754c95aeb5b691c5b26c7fd9b088b8
mogamoga <mogamoga.g1thu6@gmail.com>
2024-04-02 12:55:09 +0900

    ハーバード大学

--- src/byyn/main.js
+++ src/byyn/main.js
@@ -3,9 +3,29 @@ import { createApp } from 'vue';
 const vm = {
     data() {
         return {
-
+            roomSize: 10,
+            roomSizeMin: 10,
+            roomSizeMax: 18,
         }
     },
+    methods: {
+        onFocusInputSize(event) {
+            event.target.select();
+        },
+        onBlurInputSize(event) {
+            this.roomSize = Number(event.target.value);
+            if (this.roomSize < this.roomSizeMin) {
+                this.roomSize = this.roomSizeMin;
+            }
+            else if (this.roomSize > this.roomSizeMax) {
+                this.roomSize = this.roomSizeMax;
+            }
+            event.target.value = this.roomSize;
+        },
+        onKeyDownEnterInputSize(event) {
+            event.target.blur();
+        },
+    }
 };
 
 createApp(vm).mount('#app');
