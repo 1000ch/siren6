0a9e3b08f08ad2840008931b262f3b53556c997a
mogamoga <mogamoga.g1thu6@gmail.com>
2024-04-05 04:30:10 +0900

    宣言

--- src/logic/item/byynCheck.js
+++ src/logic/item/byynCheck.js
@@ -1,5 +1,10 @@
 import { Position, TL, TR, BL, BR } from "./position";
 
+/*
+今はさっさと実装することと拡張性を重視し、全マスから全方向に投げて調べるという方針をとる。
+もちろん枝刈りすることはできるが、それを保守することや拡張性などを考慮するとわりに合わないと判断した。
+*/
+
 export function byynCheck(room, useUdewa, isTubo) {
 
 }
